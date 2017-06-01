41e2f27333c59c33e92993f060bcd4cad32d5859
amine benselim <abens241@gmail.com>
2017-06-01 03:30:11 +0100

    create and display basic timer

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
-let timer = document.getElementById("timer");
+const Timer = duration => {
+  const seconds = Math.floor(duration % 60);
+  const minutes = Math.floor(duration / 60 % 60);
+  var hours = Math.floor(duration / 3600 % 24);
+  var days = Math.floor(duration / (3600 * 24));
 
-timer.innerHTML = "hello imer";
+  return {
+    days,
+    hours,
+    minutes,
+    seconds
+  };
+};
+
+const startTimer = (className, duration) => {
+  const timer = document.getElementsByClassName(className)[0];
+
+  // get and display ending time.
+  const endTime = new Date(new Date().getTime() + duration);
+  timer.querySelector(".endTime").innerHTML = endTime.toUTCString();
+
+  //select correspanding spans
+  const days = timer.querySelector(".days");
+  const hours = timer.querySelector(".hours");
+  const minutes = timer.querySelector(".minutes");
+  const seconds = timer.querySelector(".seconds");
+
+  let remainingTime = duration;
+
+  const updateTimer = () => {
+    const t = Timer(remainingTime--);
+    days.innerHTML = t.days;
+    hours.innerHTML = ("0" + t.hours).slice(-2);
+    minutes.innerHTML = ("0" + t.minutes).slice(-2);
+    seconds.innerHTML = ("0" + t.seconds).slice(-2);
+    console.log(t);
+    if (remainingTime < 0) {
+      clearInterval(timeinterval);
+    }
+  };
+
+  //updateTimer();
+  const timeinterval = setInterval(updateTimer, 1000);
+};
+
+startTimer("timer", 10);
